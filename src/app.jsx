0fb73dbdb8beb3e59dafa8338434e6065f34b2f6
miyano <dr000w@gmail.com>
2015-06-28 16:27:37 -0400

    removed dist
    which shouldn't have been there

--- src/app.jsx
+++ src/app.jsx
@@ -1,8 +1,14 @@
 var React = require('react');
 
 var App = React.createClass({
+
+  getInitialState() {
+    return {
+      message: "Hello from state"
+    }
+  },
   render() {
-    return <h1>Hello Working</h1>
+    return <h1>{this.state.message}</h1>
   }
 });
 
