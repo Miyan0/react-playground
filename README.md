c09c131e78d027afd2a13228eacd286ac72c03b6
miyano <dr000w@gmail.com>
2015-06-28 15:06:00 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+* npm install
+* npm run watch (will execute: watchify -t babelify ./src/app.jsx -o ./dist/bundle.js -v)
---
+++ src/app.jsx
@@ -0,0 +1,14 @@
+var React = require('react');
+
+var App = React.createClass({
+  render() {
+    return <h1>Hello Working</h1>
+  }
+});
+
+
+React.render(
+    <App />,
+    document.getElementById('example')
+);
+
